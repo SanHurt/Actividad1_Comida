9d546d9303585ef5695fc5981c816c59a6ad33ca
Sandra Hurtado <sandra.hurtado16@iesdoctorbalmis.com>
2020-12-10 13:25:45 +0100

    Agregar archivos de proyecto.

---
+++ Actividad1_Comida/BanderaConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Actividad1_Comida
+{
+    class BanderaConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((string)value == "Mexicana")
+                return "Assets/mexico.png";
+            else if ((string)value == "Americana")
+                return "Assets/united_states.png";
+            else if ((string)value == "China")
+                return "Assets/china.png";
+            else
+                return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Actividad1_Comida/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Actividad1_Comida
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Plato> listaPlatos;
+        List<string> listaComboBox;
+        public MainWindow()
+        {
+            InitializeComponent();
+            listaPlatos = Plato.GetSamples(@"E:/\DAM\2DAM\DINT\UT5\Imagenes\");
+            listBox.DataContext = listaPlatos;
+            listaComboBox = new List<string>() { "Mexicana", "Americana", "China" };
+            comboBox.DataContext = listaComboBox;
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            stackpanel.DataContext = listBox.SelectedItem;
+        }
+    }
+}
